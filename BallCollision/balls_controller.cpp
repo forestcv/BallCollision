1141bf817b4535b4ecb8a4a3dbe42d0efe323298
forestcv <forest.cv@yandex.ru>
2023-01-13 16:16:59 +0300

    1. Исправлены столкновения с правой и нижней стеной; 2. Начат расчет столкновения шаров

--- BallCollision/balls_controller.cpp
+++ BallCollision/balls_controller.cpp
@@ -18,12 +18,17 @@ static bool applyWallCollision(Ball& ball, const sf::Vector2f &nextPosition,
     {
         ball.dir.x = -ball.dir.x;
         ball.p.y -= (nextPosition.y - ball.p.y) / 2;
+        if (nextPosition.x + ball.r * 2 >= fieldWidth)
+            //ball.p.x = fieldWidth - ball.r * 2;
+            ball.p.x = (fieldWidth - ball.r * 2) * 2 - nextPosition.x;
         isCollision = true;
     }
     if (nextPosition.y <= 0 || nextPosition.y + ball.r * 2 >= fieldHieght)
     {
         ball.dir.y = -ball.dir.y;
         ball.p.x -= (nextPosition.x - ball.p.x) / 2;
+        if (nextPosition.y + ball.r * 2 >= fieldHieght)
+            ball.p.y = (fieldHieght - ball.r * 2) * 2 - nextPosition.y;
         isCollision = true;
     }
     if(!isCollision)
@@ -31,6 +36,21 @@ static bool applyWallCollision(Ball& ball, const sf::Vector2f &nextPosition,
         ball.setPosition(nextPosition);
     }
     return isCollision;
+}
+
+static bool applyBallCollision(Ball& first, Ball& second)
+{
+    double distance = sqrt((first.p.x - second.p.x) * (first.p.x - second.p.x) +
+        (first.p.y - second.p.y) * (first.p.y - second.p.y));
+    if (distance <= first.r + second.r)
+    {
+        first.dir.x = -first.dir.x;
+        first.dir.y = -first.dir.y;
+        second.dir.x = -second.dir.x;
+        second.dir.y = -second.dir.y;
+        return true;
+    }
+    return false;
 }
 
 BallsController::BallsController(BallsPtr balls,
@@ -49,6 +69,13 @@ void BallsController::update(float deltaTime)
     {
         applyWallCollision(ball, nextPosition(ball, deltaTime), fieldWidth, fieldHieght);
         //ball.setPosition(nextPosition(ball, deltaTime));
+    }
+    for (int i = 0; i < balls->size() - 1; i++)
+    {
+        for (int j = i + 1; j < balls->size(); j++)
+        {
+            applyBallCollision(balls->operator[](i), balls->operator[](j));
+        }
     }
 }
 
